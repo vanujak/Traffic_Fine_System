29cba98faeea801289d4eff61b7de9fdb23d42dc
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2026-05-19 10:16:07 +0530

    update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { PrismaClient } = require('./generated/prisma');
+const { PrismaClient } = require('../generated/prisma');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { Pool } = require('pg');
 
